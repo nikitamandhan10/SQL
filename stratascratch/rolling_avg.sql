2fbe137f92880112394e379ebdcbceff7cf14263
Nikita Mandhan <124109164+nikitamandhan10@users.noreply.github.com>
2026-03-12 16:13:46 -0700

    Update rolling_avg.sql

--- stratascratch/rolling_avg.sql
+++ stratascratch/rolling_avg.sql
@@ -10,3 +10,48 @@ purchase_amt: bigint
 user_id: bigint
 
 '''
+
+
+-- with monthly_revenue as 
+-- (select date_trunc('month', created_at) as month,
+-- sum(purchase_amt) as total_revenue
+-- from amazon_purchases
+-- WHERE purchase_amt > 0
+-- group by date_trunc('month', created_at))
+
+-- select to_char(month, 'yyyy-mm') as year_mth,
+-- round(avg(total_revenue) over(order by month rows between 2 preceding and current row),2) as rolling_avg_3m
+-- from monthly_revenue
+
+
+-- Where some months might be missing
+WITH min_max_mth AS (
+    SELECT 
+        DATE_TRUNC('month', MIN(created_at)) AS min_month,
+        DATE_TRUNC('month', MAX(created_at)) AS max_month
+    FROM amazon_purchases
+),
+
+all_months AS (
+    SELECT GENERATE_SERIES(min_month, max_month, INTERVAL '1 month') AS month
+    FROM min_max_mth
+),
+
+monthly_revenue_cal AS (
+    SELECT 
+        DATE_TRUNC('month', created_at) AS month,
+        SUM(purchase_amt) AS total_rev
+    FROM amazon_purchases
+    WHERE purchase_amt > 0
+    GROUP BY DATE_TRUNC('month', created_at)
+)
+
+SELECT 
+    TO_CHAR(a.month, 'YYYY-MM') AS month,
+    AVG(COALESCE(total_rev,0)) OVER (
+        ORDER BY a.month 
+        ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ) AS rolling_avg_3m
+FROM all_months a
+LEFT JOIN monthly_revenue_cal m USING(month)
+ORDER BY a.month;
