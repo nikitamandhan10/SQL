93276d05f304bcf86a90ec17fe6039fcfe830b22
Nikita Mandhan <124109164+nikitamandhan10@users.noreply.github.com>
2026-03-21 14:15:02 -0700

    Create Same_Day_Orders.sql

---
+++ stratascratch/Same_Day_Orders.sql
@@ -0,0 +1,18 @@
+'''
+Identify users who started a session and placed an order on the same day.
+For these users, return the total number of orders placed on that day and the total order value for that day.
+Your output should include the user_id, the session_date, the total number of orders, and the total order value for that day.
+
+Tables
+sessions
+session_date: date 
+session_id: bigint
+user_id: bigint
+
+order_summary
+order_date: date
+order_id: bigint
+order_value: bigint
+user_id: bigint
+'''
+
